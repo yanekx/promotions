d959cac96ff5b45a4e67ee0b495b2c7652af9145
Yan Matveichuk <yan.summer23@gmail.com>
2021-05-02 16:39:29 +0300

    Fix rubocop violations in declaration item class

--- lib/promotions/declaration/item.rb
+++ lib/promotions/declaration/item.rb
@@ -1,22 +1,26 @@
-require_relative '../types'
+# frozen_string_literal: true
+
+require_relative "../types"
 
 module Promotions
   class Declaration
     class Item
       def self.create_item(block)
-        self::new.tap { |item| item.instance_eval(&block) }
+        new.tap { |item| item.instance_eval(&block) }
       end
 
       def self.const_missing(name)
         return Promotions::Types.const_get(name) if Promotions::Types.check_type(name)
+
         super
       end
 
-      def method_missing(method_name, *args)
-        type_name = args.first
-        instance_variable_set(:@type, type_name)
-        
+      def method_missing(method_name, *args, &block)
+        instance_variable_set(:@type, args.first)
+
         define_singleton_method(method_name) { self }
+      rescue NameError
+        super
       end
     end
   end
