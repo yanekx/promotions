4bf4d6ec110ca78d596021d4695c62512ba8d73f
Yan Matveichuk <yan.summer23@gmail.com>
2021-05-02 15:58:54 +0300

    Add currency declaration

---
+++ lib/promotions/declaration/currency.rb
@@ -0,0 +1,58 @@
+require 'byebug'
+
+module Promotions
+  class Declaration
+    class Currency
+      STRING_KEYS = [
+        :iso_numeric,
+        :iso_code,
+        :name,
+        :symbol,
+        :decimal_mark,
+        :thousands_separator
+      ].freeze
+
+      attr_reader :currency_config
+
+      def initialize
+        @currency_config = {}
+      end
+
+      def self.create_currency(block)
+        self::new.tap { |currency| currency.instance_eval(&block) }          
+      end
+
+      def method_missing(method_name, *args)
+        type_name = args.first
+        
+        parse_config_value(method_name, args.first)
+      end
+
+      private 
+
+      def parse_config_value(method_name, method_value)
+        case [method_name, method_value]
+        in Symbol => name, Integer => value
+          parse_integer_value(name, value)
+        in Symbol => name, String => value
+          parse_string_value(name, value)
+        end
+      end
+
+      def parse_integer_value(name, value)
+        case[name, value]
+        in :priority, value
+          @currency_config[:priority] = value
+        in :subunit_to_unit, value
+          @currency_config[:priority] = value
+        end
+      end
+
+      def parse_string_value(name, value)        
+        if STRING_KEYS.include?(name)
+          @currency_config[name] = value
+        end
+      end
+    end
+  end
+end
